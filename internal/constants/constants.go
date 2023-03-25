2211331b9808a23c3d7c23ce3931362bd5b9faba
marhjoh <marhjoh@stud.ntnu.no>
2023-03-25 12:54:54 +0100

    Add: constants.go

---
+++ internal/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+// This file defines constants used throughout the program.
+const (
+	// PORT Default port. If the port is not set by environment variables, set the port.
+	DEFAULT_PORT = "8080"
+
+	// The paths that will be handled by each handler
+	DEFAULT_PATH       = "/energy/"
+	CURRENT_PATH       = "/energy/v1/renewables/current/"
+	HISTORY_PATH       = "/energy/v1/renewables/history/"
+	STATUS_PATH        = "/energy/v1/status/"
+	NOTIFICATIONS_PATH = "/energy/v1/notifications/"
+)
