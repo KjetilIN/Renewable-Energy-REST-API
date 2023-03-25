e799cf35f474daf736d83ac138ad0ce8a9ff55fb
marhjoh <marhjoh@stud.ntnu.no>
2023-03-25 13:49:14 +0100

    Add: init.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
+	"assignment-2/internal/constants"
+	"assignment-2/internal/webserver"
 	"log"
-	"net/http"
 	"os"
 )
 
 // The main function handles ports assignment, sets up handler endpoints and starts the HTTP-server
 func main() {
-
 	// Handle port assignment (either based on environment variable, or local override)
 	port := os.Getenv("PORT")
-	defaultPort := 8080
 	if port == "" {
-		log.Println("PORT has not been set. Default: 8080", defaultPort)
+		log.Println("PORT has not been set. Default: 8080", constants.DEFAULT_PORT)
 	}
-
-	// Set up handler endpoints
-
-	// Starting HTTP-server
-	log.Println("Starting server on port " + port + "...")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	webserver.InitServer()
 }
---
+++ internal/webserver/init.go
@@ -0,0 +1,25 @@
+package webserver
+
+import (
+	"assignment-2/internal/constants"
+	"assignment-2/internal/webserver/handlers"
+	"assignment-2/internal/webserver/uptime"
+	"log"
+	"net/http"
+)
+
+// InitServer sets up handler endpoints and starts the HTTP-server
+func InitServer() {
+
+	// Points the different URL-paths to the correct handler
+	http.HandleFunc(constants.DEFAULT_PATH, handlers.HandlerDefault)
+	http.HandleFunc(constants.CURRENT_PATH, handlers.HandlerCurrent)
+	http.HandleFunc(constants.HISTORY_PATH, handlers.HandlerHistory)
+	http.HandleFunc(constants.STATUS_PATH, handlers.HandlerStatus)
+	http.HandleFunc(constants.NOTIFICATIONS_PATH, handlers.HandlerNotifications)
+
+	// Starting HTTP-server
+	log.Println("Starting server on port " + constants.DEFAULT_PORT + " ...")
+	uptime.Init()
+	log.Fatal(http.ListenAndServe(":"+constants.DEFAULT_PORT, nil))
+}
