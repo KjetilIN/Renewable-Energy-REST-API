a72e84ad6e00695c55b2fcffaa79d197699356c4
Sander <sandeth@stud.ntnu.no>
2023-03-29 13:41:51 +0200

    Add: endpoint working. Known Error: issue #18

--- internal/webserver/handlers/history.go
+++ internal/webserver/handlers/history.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"assignment-2/internal/utility"
 	"assignment-2/internal/webserver/structs"
 	"encoding/csv"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -12,28 +14,35 @@ import (
 func HandlerHistory(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
+	listOfRSE, jsonError := rseToJSON()
+	if jsonError != nil {
+		http.Error(w, jsonError.Error(), http.StatusInternalServerError)
+	}
+	utility.Encoder(w, listOfRSE)
 }
 
 // rseToJSON is an internal function to use a 2D string and input it into a struct.
 func rseToJSON() ([]structs.HistoricalRSE, error) {
 	var jsonList []structs.HistoricalRSE
 	// readFromFile is a 2D string array.
-	readFromFile, readErr := readCSV("/internal/res/renewable-share-energy.csv")
+	readFromFile, readErr := readCSV("./internal/res/renewable-share-energy.csv")
 	if readErr != nil {
 		return nil, readErr
 	}
 	var lineRead []string
 	//Iterates through 1 dimension of readFromFile.
-	for i := 0; i < len(readFromFile); i++ {
+	for i := 1; i < len(readFromFile); i++ {
 		// Stores a slice of values to be iterated through.
 		lineRead = readFromFile[i]
 
 		year, convErr := strconv.Atoi(lineRead[2]) // Converts string line to integer.
 		if convErr != nil {
+			log.Fatal(convErr)
 			return nil, convErr
 		}
 		percentage, convErr := strconv.ParseFloat(lineRead[3], 6) // Converts string line to float og 6 decimals.
 		if convErr != nil {
+			log.Fatal(convErr)
 			return nil, convErr
 		}
 		// Iterates through the lineRead slice, and appends to a new entity in HistoricalRSE slice.
