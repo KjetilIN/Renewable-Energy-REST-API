fc482191ba11e473adeefd51f35be355ffee6013
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-04-01 23:34:53 +0200

    add: implemented all GET methods for issue #24

--- internal/webserver/handlers/notifications.go
+++ internal/webserver/handlers/notifications.go
@@ -4,6 +4,7 @@ import (
 	"assignment-2/internal/constants"
 	"assignment-2/internal/webserver/structs"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"strings"
@@ -18,55 +19,72 @@ func InitWebhookRegistrations(){
 }
 
 
+// Fetch a webhook using its ID
+func fetchWebhookWithID(id string) (structs.WebhookID, error) {
+	for _, hook := range webhooks{
+		if(hook.ID == id){
+			return hook, nil;
+		}
+	}
+	return structs.WebhookID{}, errors.New("Fetch with id: could not find")
+}
+
+//Fetch all webhooks
+func fetchAllWebhooks() ([]structs.WebhookID, error){
+	// At this stage we already have all the webhooks 
+	return webhooks, nil
+}
+
+
+// Function for handling get request for the  
 func handleGetMethod(w http.ResponseWriter, r *http.Request){
 	//Get any parameter received 
 	givenParameters := strings.TrimPrefix(r.URL.Path, constants.NOTIFICATIONS_PATH)
 	urlParts := strings.Split(givenParameters, "/")
+
 	
 	//Should only be given max one parameters 
 	if (len(urlParts) > 1){
 		log.Println("Error on get method for notification endpoint")
 		http.Error(w, "Not correct usage of getting webhook information", http.StatusBadRequest)
 		return;
-	}else{
-		//Get the webhook if there is an id 
-		if (len(urlParts) == 1){
-
-			//An id was given 
-			ID := urlParts[0]
-
-			//Loop through and try to find the webhook with the given id
-			var foundWebhook structs.WebhookID
-			for _, hook := range webhooks{
-				if(hook.ID == ID){
-					foundWebhook = hook
-					break // found the hook, and break out of loop 
-				}
-			}
-
-			//check if we found the webhook
-			if(len(foundWebhook.ID) == 0 ){
-				err := json.NewEncoder(w).Encode(&foundWebhook)
-				if(err != nil){
-					return
-				}
-
-			}else{
-				//Encode the result 
-				err := json.NewEncoder(w).Encode(&foundWebhook)
-				if(err != nil){
-					return
-				}
-			}
-
-			
-		}else{
-
-		}
+	}else if (len(urlParts) == 1){
+		//Fetch only the webhook with 
+		fetchedWebhook, err := fetchWebhookWithID(urlParts[0])
 		
+		//Error on fetching the webhook with the id
+		if(err != nil){
+			http.Error(w, "No existing webhook with the ID: " + urlParts[0], http.StatusNotFound)
+			return
+		}
+
+		//Encode the result
+		encodeError := json.NewEncoder(w).Encode(&fetchedWebhook)
+		if(encodeError != nil){
+			log.Println("Error on encoding fetched webhook with ID", encodeError.Error())
+			http.Error(w, "Error on encoding struct", http.StatusInternalServerError)
+			return 
+		}
+			
+	}else{
+		// Fetch all webhooks 
+		allWebHooks, fetchError := fetchAllWebhooks();
+
+		//Handle error 
+		if(fetchError != nil){
+			log.Println("Error on fetching all webhooks: ", fetchError.Error())
+			http.Error(w, "Could not fetch all webhooks", http.StatusInternalServerError)
+			return 
+		}
 
 
-
+		//Encode the result
+		encodeError := json.NewEncoder(w).Encode(&allWebHooks)
+		if(encodeError != nil){
+			log.Println("Error on encoding fetched webhooks", encodeError.Error())
+			http.Error(w, "Error on encoding struct", http.StatusInternalServerError)
+			return 
+		}
 		
 	}
 
@@ -84,9 +102,9 @@ func HandlerNotifications(w http.ResponseWriter, r *http.Request) {
 		case http.MethodDelete:
 			break
 		case http.MethodGet:
+			//Handle get request to the endpoint based on given parameters 
 			handleGetMethod(w,r)
 			break
-
 		default:
 			// Only allowed methods
 			http.Error(w, "Method " + r.Method + "not supported", http.StatusMethodNotAllowed)
