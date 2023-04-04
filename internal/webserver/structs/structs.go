91fcb3bc291a7bd8fce083f59da8c0a97d48faed
marhjoh <marhjoh@stud.ntnu.no>
2023-04-04 10:34:29 +0200

    add: structs for webhooks

--- internal/webserver/structs/structs.go
+++ internal/webserver/structs/structs.go
@@ -2,9 +2,24 @@ package structs
 
 // This file defines structs to work with data.
 type Status struct {
-	CasesApi       int    `json:"countries_api"`
+	CountryApi     int    `json:"countries_api"`
 	NotificationDB int    `json:"notification_db"`
 	Webhooks       int    `json:"webhooks"`
 	Version        string `json:"version"`
 	Uptime         int    `json:"uptime"`
+}
+
+type Webhook struct {
+	Url     string `json:"url"`
+	Country string `json:"country"`
+	Calls   int    `json:"calls"`
+}
+
+type WebhookID struct {
+	ID string `json:"webhook_id"`
+	Webhook
+}
+
+type IdResponse struct {
+	ID string `json:"webhook_id"`
 }
