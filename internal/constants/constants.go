bb0ea5210b27e43c59abf26c5af98af7897c94b5
marhjoh <marhjoh@stud.ntnu.no>
2023-04-04 11:01:58 +0200

    fix: changed countryApi url

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -12,5 +12,5 @@ const (
 	STATUS_PATH        = "/energy/v1/status/"
 	NOTIFICATIONS_PATH = "/energy/v1/notifications/"
 
-	COUNTRIES_API = "https://restcountries.com/v2/"
+	COUNTRIES_API = "https://restcountries.com/v3.1/"
 )
