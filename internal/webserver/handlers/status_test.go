3f76570bb17fe1210575875c105a59810c8d397f
marhjoh <marhjoh@stud.ntnu.no>
2023-04-06 16:44:06 +0200

    add: test file status_test.go structure for status.go

---
+++ internal/webserver/handlers/status_test.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"testing"
+)
+
+func TestHandlerStatus_ValidMethod(t *testing.T) {
+}
+
+func TestHandlerStatus_InvalidMethod(t *testing.T) {
+}
+
+func TestHandlerStatus_GetStatusError(t *testing.T) {
+}
+
+func TestHandlerStatus_GetStatusSuccess(t *testing.T) {
+}
+
+func TestHandlerStatus_JSONEncoding(t *testing.T) {
+}
+
+func TestHandlerStatus_UnavailableThirdParty(t *testing.T) {
+}
