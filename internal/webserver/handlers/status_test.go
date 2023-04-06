26da1bfc2b7140db9fe9b7e574adf18ef39c8641
marhjoh <marhjoh@stud.ntnu.no>
2023-04-06 16:51:33 +0200

    add: test function TestHandlerStatus_ValidMethod

--- internal/webserver/handlers/status_test.go
+++ internal/webserver/handlers/status_test.go
@@ -1,10 +1,29 @@
 package handlers
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
+// TestHandlerStatus_ValidMethod checks if the correct HTTP status code is returned by the handler
+// when using a valid method (GET).
+// Returns: http.StatusOK, or an error message.
 func TestHandlerStatus_ValidMethod(t *testing.T) {
+	InitWebhookRegistrations()
+
+	req, err := http.NewRequest("GET", "/status", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerStatus)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
 }
 
 func TestHandlerStatus_InvalidMethod(t *testing.T) {
