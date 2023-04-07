26515756d634d31d8f8617c6956260e1e367cb84
marhjoh <marhjoh@stud.ntnu.no>
2023-04-07 22:38:25 +0200

    add: test function TestHandlerStatus_GetStatusError

--- internal/webserver/handlers/status_test.go
+++ internal/webserver/handlers/status_test.go
@@ -1,10 +1,19 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+type mockRoundTripper struct {
+	roundTripFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (m *mockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	return m.roundTripFunc(req)
+}
 
 // TestHandlerStatus_ValidMethod checks if the correct HTTP status code is returned by the handler
 // when using a valid method (GET).
@@ -45,7 +54,27 @@ func TestHandlerStatus_InvalidMethod(t *testing.T) {
 	}
 }
 
+// TestHandlerStatus_GetStatusError checks if the getStatus() function returns an error when accessing the country API fails.
+// Returns: an error.
 func TestHandlerStatus_GetStatusError(t *testing.T) {
+	InitWebhookRegistrations()
+
+	// Create a mock http client that returns an error when accessing the country API
+	mockErrClient := &http.Client{
+		Transport: &mockRoundTripper{
+			roundTripFunc: func(*http.Request) (*http.Response, error) {
+				return nil, errors.New("error accessing country API")
+			},
+		},
+	}
+
+	// Replace the global client with the mock client for this test case
+	client = mockErrClient
+
+	_, err := getStatus()
+	if err == nil {
+		t.Error("getStatus() did not return an error when accessing country API failed")
+	}
 }
 
 func TestHandlerStatus_GetStatusSuccess(t *testing.T) {
