77911f05b7b8be8b4b484a7cb381270054780257
marhjoh <marhjoh@stud.ntnu.no>
2023-04-25 19:12:34 +0200

    add: information query for status endpoint

--- internal/constants/endpointQueries.go
+++ internal/constants/endpointQueries.go
@@ -2,7 +2,6 @@ package constants
 
 import "assignment-2/internal/webserver/structs"
 
-
 var HISTORY_QUERIES = []structs.Information{
 	{
 		Title:   "No parameters",
@@ -10,18 +9,20 @@ var HISTORY_QUERIES = []structs.Information{
 		Description: "When you use endpoint with no parameters or queries, it will print all historical countries' mean value from" +
 			"renewable share energy, which contains the percentage of renewable energy sources a given country has.",
 	},
-	
 }
-
 
 var STATUS_QUERIES = []structs.Information{
-
+	{
+		Title:   "No parameters",
+		Example: "Use: /energy/v1/status",
+		Description: "This endpoint are used with no parameters or queries, it will print all information about the " +
+			"availability of all individual services this service depends on. The reporting occurs based on status " +
+			"codes returned by the dependent services. The status interface further provides information about the " +
+			"number of registered webhooks, and the uptime of the service. It also provides the total memory usage " +
+			"of the computer in use.",
+	},
 }
 
-var NOTIFICATION_QUERIES = []structs.Information{
-	
-}
+var NOTIFICATION_QUERIES = []structs.Information{}
 
-var CURRENT_QUERIES = []structs.Information{
-	
-}
+var CURRENT_QUERIES = []structs.Information{}
