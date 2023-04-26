f741e6822d7e7e231b40b45373f701f34d7b45fa
marhjoh <marhjoh@stud.ntnu.no>
2023-04-27 00:12:05 +0200

    remove: documented code

--- internal/webserver/structs/structs.go
+++ internal/webserver/structs/structs.go
@@ -4,12 +4,11 @@ import "time"
 
 // Status This file defines structs to work with data.
 type Status struct {
-	CountriesApi   int    `json:"countries_api"`
-	NotificationDB int    `json:"notification_db"`
-	Webhooks       int    `json:"webhooks"`
-	Version        string `json:"version"`
-	Uptime         string `json:"uptime"`
-	//AverageSystemLoad string `json:"average_system_load"`
+	CountriesApi     int    `json:"countries_api"`
+	NotificationDB   int    `json:"notification_db"`
+	Webhooks         int    `json:"webhooks"`
+	Version          string `json:"version"`
+	Uptime           string `json:"uptime"`
 	TotalMemoryUsage string `json:"total_memory_usage"`
 }
 
